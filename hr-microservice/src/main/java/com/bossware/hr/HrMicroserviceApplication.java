00be11d6b2004c78f24d1ce987bf19045f8dfe0b
Ahmet Tarık Bostan <a.tarikbostan@gmail.com>
2022-09-17 17:42:20 +0300

    config add

--- hr-microservice/src/main/java/com/bossware/hr/HrMicroserviceApplication.java
+++ hr-microservice/src/main/java/com/bossware/hr/HrMicroserviceApplication.java
@@ -1,12 +1,17 @@
 package com.bossware.hr;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HrMicroserviceApplication {
 
+	@Value("{$spring.datasource.url}")
+	public static String configUrlTrace;
+	
 	public static void main(String[] args) {
+		System.out.println("Config trace %s".formatted(configUrlTrace));
 		SpringApplication.run(HrMicroserviceApplication.class, args);
 	}
 
