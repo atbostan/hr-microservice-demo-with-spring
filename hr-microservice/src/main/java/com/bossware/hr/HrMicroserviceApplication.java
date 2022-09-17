4561ae30813657bf567940143eeedac8acf92e0e
Ahmet Tarık Bostan <a.tarikbostan@gmail.com>
2022-09-17 17:48:01 +0300

    add config - 3

--- hr-microservice/src/main/java/com/bossware/hr/HrMicroserviceApplication.java
+++ hr-microservice/src/main/java/com/bossware/hr/HrMicroserviceApplication.java
@@ -3,8 +3,10 @@ package com.bossware.hr;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
 public class HrMicroserviceApplication {
 
 	@Value("${spring.datasource.url}")
